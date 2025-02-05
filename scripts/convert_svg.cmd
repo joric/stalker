91bda8eaf0903f941460ffbcc7443262289a8c19
joric <joric@users.noreply.github.com>
2025-02-05 06:46:31 +0500

    updated files

--- scripts/convert_svg.cmd
+++ scripts/convert_svg.cmd
@@ -1,5 +1,6 @@
 @echo off
+rem npm install svg2geojson
 
-svg2geojson T_Regions_Map.svg -d && move T_Regions_Map.geojson ..\data\regions.json
+svg2geojson T_Regions_Map.svg -d -o > ..\data\regions.json
 
 
