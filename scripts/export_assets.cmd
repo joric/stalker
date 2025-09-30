aae311032d39888304091242f5cff7d6bdeef89f
joric <joric@users.noreply.github.com>
2025-09-30 06:43:50 +0500

    updated files

--- scripts/export_assets.cmd
+++ scripts/export_assets.cmd
@@ -4,6 +4,8 @@ rem download Ue4Export here: https://github.com/CrystalFerrai/Ue4Export
 rem assetlist.txt must have [Text] as a first line (ini header) to export files as json
 
 set exe=D:\Shared\Tools\Hacking\Games\UE\Ue4Export\Ue4Export.exe
+rem set exe=C:\Temp\github\Ue4Export\Ue4Export\bin\Release\net8.0\win-x64\publish\Ue4Export.exe
+
 set gamePath=E:\Games\S.T.A.L.K.E.R. 2.Heart.of.Chornobyl.Ultimate.Editon-InsaneRamZes\
 set mappings="%gamePath%\Stalker2.usmap"
 set paks="%gamePath%\Stalker2\Content\Paks"
@@ -12,11 +14,8 @@ set options=--mix-output --key %key% --mappings %mappings% %paks% UE5_1
 
 set out=C:\Temp\Exports
 
-if not exist "%out%\Stalker2\Content\_Stalker_2\maps\_Stalker2_WorldMap\WorldMap_WP.json" (
-	%exe% %options% %~dp0\assetlist.txt %out%
-)
+%exe% %options% %~dp0\assetlist.txt %out%
 
 if exist %~dp0\missing_files.txt (
 	%exe% %options% %~dp0\missing_files.txt %out%
 )
-
