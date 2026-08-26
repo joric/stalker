3b5e5bb66638f88d26628f37be5b00e3ec07e251
joric <joric@users.noreply.github.com>
2026-08-26 08:16:31 +0500

    updated files

--- scripts/convert_bin.cmd
+++ scripts/convert_bin.cmd
@@ -1,3 +1,3 @@
 python bin2cfg.py C:\Temp\Exports\Stalker2\Content\GameLite\GameData
-rem python bin2cfg.py C:\Temp\Exports\Stalker2\Content\GameLite\DLCGameData
+python bin2cfg.py C:\Temp\Exports\Stalker2\Content\GameLite\DLCGameData
 
